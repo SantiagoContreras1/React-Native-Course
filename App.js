1adf006ed6a485e9b838ebdeca6ab058ed7bdc92
SantiagoContreras1 <mcontreras-2023555@kinal.edu.gt>
2025-06-15 17:17:19 -0600

    Bottom Tab Project

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text> HomeScreen </Text>
     </View>
+  )
+}
+
+function NewScreen() {
+  return (
+    <View style={styles.container}>
+      <Text> New Screen </Text>
+    </View>
+  )
+}
+
+function SettingsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text> Settings Screen </Text>
+    </View>
+  )
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'New') {
+              iconName = focused ? 'car' : 'car-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Icon name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'green',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="New" component={NewScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
 
